2c04a56305863ef781a5ec3d2bc020c6646088e5
mdstev1 <michaeldstevens1@gmail.com>
2020-03-05 12:20:33 -0700

    First commit

---
+++ tethysapp/all_about_me/app.py
@@ -0,0 +1,34 @@
+from tethys_sdk.base import TethysAppBase, url_map_maker
+
+
+class AllAboutMe(TethysAppBase):
+    """
+    Tethys app class for All About Me.
+    """
+
+    name = 'All About Me'
+    index = 'all_about_me:home'
+    icon = 'all_about_me/images/icon.gif'
+    package = 'all_about_me'
+    root_url = 'all-about-me'
+    color = '#f39c12'
+    description = 'This app provides a brief description of the author.'
+    tags = ''
+    enable_feedback = False
+    feedback_emails = []
+
+    def url_maps(self):
+        """
+        Add controllers
+        """
+        UrlMap = url_map_maker(self.root_url)
+
+        url_maps = (
+            UrlMap(
+                name='home',
+                url='all-about-me',
+                controller='all_about_me.controllers.home'
+            ),
+        )
+
+        return url_maps
